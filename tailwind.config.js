a13adbd09faad7f0f48d6fe2c0a5adc925156c72
inkrafi <rafiferdian7@gmail.com>
2022-05-21 08:25:16 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}', 'index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#45aaf2',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
